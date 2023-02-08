0d2ab7b578bc6c51ba5878809b82f413cff25749
SapheSaber <aac2@andrew.cmu.edu>
2023-02-08 18:49:46 -0500

    added a popup box

--- myworld/src/screens/Home.js
+++ myworld/src/screens/Home.js
@@ -1,5 +1,7 @@
 import './Home.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import Modal from '@material-ui/core/Modal';
+
 
 import islandPic from '../img/island1.png'
 import islandPic2 from '../img/island1.png'
@@ -13,7 +15,7 @@ import { Link } from 'react-router-dom';
 function Home() {
   const [popUp, setPopUp] = useState(false)
   const popUpHandler = () => {
-    setPopUp(true)
+    setPopUp(!popUp)
   }
 
   return (
@@ -52,16 +54,33 @@ function Home() {
 
       </div>
 
-      <div className = "mapImg">
+      <div className="mapImg">
         <div className="map">
-        <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Pittsburgh+(Carnegie%20Mellon%20University)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-        <p>Look up upcoming Events!</p>
+          <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Pittsburgh+(Carnegie%20Mellon%20University)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+          <p>Look up upcoming Events!</p>
 
         </div>
       </div>
 
       <div className='buttonContainer'>
-        <div className="button" onClick={popUpHandler}> <FaPlus color='#ffffff' /> </div>
+        <div className="button" onClick={popUpHandler}> <FaPlus color='#ffffff' />
+          <Modal className="popUp"
+            open={popUp}
+          >
+            <div className="window">
+              <div className="form">
+                <h2>What's on your mind?</h2>
+                <h3>Name of your proposed event</h3>
+                <h3>Event Date</h3>
+                <h3>Event Location</h3>
+
+              </div>
+              <button className="button-pop" onClick={popUpHandler}>
+                <h2>Close</h2>
+              </button>
+            </div>
+          </Modal>
+        </div>
       </div>
 
       <div class='box'>
